bf057eb6208fa98c0cff100f51d6ea16ed077bf9
HarishRahul <harishrahults@gmail.com>
2024-07-14 22:17:19 +0530

    initial commit

---
+++ Credit-Card-Fraud-Detection-main/app.py
@@ -0,0 +1,55 @@
+from fastapi import FastAPI, File, Query, UploadFile, HTTPException, Form
+from fastapi.responses import FileResponse, PlainTextResponse
+import uvicorn
+import pickle
+import numpy as np
+from pydantic import BaseModel
+
+
+
+app = FastAPI(
+    title="Credit Card Fraud Detection API",
+    description="""An API that utilises a Machine Learning model that detects if a credit card transaction is fraudulent or not based on the following features: hours, amount, transaction type etc.""",
+    version="1.0.0", debug=True)
+
+
+with open('credit_fraud.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+
+@app.get("/", response_class=PlainTextResponse)
+async def running():
+  note = """
+Credit Card Fraud Detection API 🙌🏻
+
+Note: add "/docs" to the URL to get the Swagger UI Docs or "/redoc"
+  """
+  return note
+
+favicon_path = 'favicon.png'
+@app.get('/favicon.png', include_in_schema=False)
+async def favicon():
+    return FileResponse(favicon_path)
+																	
+class fraudDetection(BaseModel):
+    step:int
+    types:int
+    amount:float	
+    oldbalanceorig:float	
+    newbalanceorig:float	
+    oldbalancedest:float	
+    newbalancedest:float	
+    isflaggedfraud:float
+
+
+@app.post('/predict')
+def predict(data : fraudDetection):                                                                                                                                                                                                    
+    features = np.array([[data.step, data.types, data.amount, data.oldbalanceorig, data.newbalanceorig, data.oldbalancedest, data.newbalancedest, data.isflaggedfraud]])
+    with open('credit_fraud.pkl', 'rb') as file:
+        model = pickle.load(file)
+    predictions = model.predict(features)
+    print(predictions)
+    if predictions == 1:
+        return {"fraudulent"}
+    elif predictions == 0:
+        return {"not fraudulent"}
